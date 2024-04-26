7ecdfff8799a10d3000c79aec225eeb21f324584
Alex <alex.chenqr@gmail.com>
2024-04-26 11:40:07 +0800

    add

--- src/main/java/com/chenqr/InspirationalBot/App.java
+++ src/main/java/com/chenqr/InspirationalBot/App.java
@@ -2,6 +2,7 @@ package com.chenqr.InspirationalBot;
 
 import org.springframework.boot.SpringApplication;
 
+@SpringApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
